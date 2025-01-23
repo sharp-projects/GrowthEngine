34c20cdd8f90ef5f3cc59b8c3dbb27556987c3db
Kevin Sharp <sharp.projects.101@gmail.com>
2025-01-23 09:56:50 -0700

    Second round of exercises

---
+++ Exercism/go/cars-assemble/cars_assemble.go
@@ -0,0 +1,25 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	return float64(productionRate) * (successRate / 100)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	const bundleSize = 10
+	const costPerBundle = 95000
+	const costPerUnit = 10000
+
+	bundles := carsCount / bundleSize
+	individual := carsCount % 10
+
+	return uint(bundles*costPerBundle + individual*costPerUnit)
+}
